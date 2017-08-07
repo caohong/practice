b41b5825a548e16bb179d1484e95c64520c9a6d9
caohong <caohong@le.com>
2017-08-07 11:02:01 +0800

    lintcode: unique paths, (2nd try, improve performance)
    Signed-off-by: caohong <caohong@le.com>

--- leetcode/README.md
+++ leetcode/README.md
@@ -6,3 +6,4 @@
 |8|[Rotate String](http://www.lintcode.com/en/problem/)|[C++](./c++/lintcode-8-Rotate-String/)|Easy|
 |13|[strStr](http://www.lintcode.com/en/problem/strstr/)|[C++](./c++/lintcode-13-strstr/)|Easy|
 |14|[First Position of Target](http://www.lintcode.com/en/problem/first-position-of-target/)|[C++](./c++/lintcode-14-first-pos-of-target/)|Easy|
+|114|[Unique Path](http://www.lintcode.com/en/problem/unique-paths/)|[C++](./c++/lintcode-114-unique-paths)|Easy|
--- leetcode/c++/lintcode-114-unique-paths/unique-paths.cpp
+++ leetcode/c++/lintcode-114-unique-paths/unique-paths.cpp
@@ -9,12 +9,12 @@ public:
      * @return an integer
      */
     int uniquePaths(int m, int n) {
-        if ((m < 1) || (n < 1)) {
-            return 0;
+        vector<vector<int> > result(m, vector<int>(n, 1));
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                result[i][j] = result[i - 1][j] + result[i][j - 1];
+            }
         }
-        if ((m == 1) || (n == 1)) {
-            return 1;
-        }
-        return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+        return result[m - 1][n - 1];
     }
 };
