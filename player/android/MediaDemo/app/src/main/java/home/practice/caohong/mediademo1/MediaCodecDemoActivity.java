2346c4b4291913d4fa11d11991ec5836e5ee9638
caohong <caohong@le.com>
2017-08-20 23:26:00 +0800

    player: MediaCodec: use getOutputImage() to get the frame
    Signed-off-by: caohong <caohong@le.com>

--- player/android/MediaDemo/app/src/main/java/home/practice/caohong/mediademo1/MediaCodecDemoActivity.java
+++ player/android/MediaDemo/app/src/main/java/home/practice/caohong/mediademo1/MediaCodecDemoActivity.java
@@ -1,6 +1,8 @@
 package home.practice.caohong.mediademo1;
 
 import android.app.Activity;
+import android.media.Image;
+import android.media.MediaCodec;
 import android.media.MediaExtractor;
 import android.media.MediaFormat;
 import android.os.Bundle;
@@ -8,6 +10,7 @@ import android.util.Log;
 
 import java.io.File;
 import java.nio.ByteBuffer;
+
 
 public class MediaCodecDemoActivity extends Activity {
 
@@ -19,36 +22,56 @@ public class MediaCodecDemoActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         try {
+            //
+            // read a sample
             MediaExtractor extractor = new MediaExtractor();
             String videoFilePath = "/sdcard/Movies/480-WildLife.mp4";
+            MediaFormat format = null;
+            String mime = "";
             File file = new File(videoFilePath);
             extractor.setDataSource(file.toString());
             int i = 0;
             for (; i < extractor.getTrackCount(); i++) {
-                MediaFormat format = extractor.getTrackFormat(i);
-                String mime = format.getString(MediaFormat.KEY_MIME);
+                format = extractor.getTrackFormat(i);
+                mime = format.getString(MediaFormat.KEY_MIME);
                 if (mime.startsWith("video/")) {
                     extractor.selectTrack(i);
                     break;
                 }
             }
 
-            ByteBuffer byteBuf = ByteBuffer.allocate(1024 * 1024);
-            extractor.readSampleData(byteBuf, 0);
+            //
+            // instantiate a decoder
+            MediaCodec decoder = MediaCodec.createDecoderByType(mime);
+            decoder.configure(format, null, null, 0);
+            MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+            decoder.start();
 
-            dumpByteBuffer(byteBuf);
 
+            //
+            // try to get image
+            for (;;) {
+                int inputBufferId = decoder.dequeueInputBuffer(0);
+                if (inputBufferId >= 0) {
+                    ByteBuffer inputBuffer = decoder.getInputBuffer(inputBufferId);
+                    int sampleSize = extractor.readSampleData(inputBuffer, 0);
+                    if (extractor.advance() && sampleSize > 0) {
+                        decoder.queueInputBuffer(inputBufferId, 0, sampleSize, extractor.getSampleTime(), 0);
+                    } else {
+                        Log.d(TAG, "input buffer EOS");
+                    }
+                }
+
+                int outputBufferId = decoder.dequeueOutputBuffer(bufferInfo, 0);
+                if (outputBufferId >= 0) {
+                    Image image = decoder.getOutputImage(outputBufferId);
+                    Log.d(TAG, "image format: " + image.getFormat());
+                    image.close();
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private void dumpByteBuffer(ByteBuffer byteBuf) {
-        StringBuilder strBuilder = new StringBuilder();
-        for (int i = 0; i < 16; i++) {
-            strBuilder.append(" ").append(byteBuf.get());
-        }
-        Log.d(TAG, "buffer: " + strBuilder.toString());
     }
 }
 
--- player/android/README.md
+++ player/android/README.md
@@ -2,3 +2,5 @@ Practice for Video Player Development (Android)
 ===========
 
 1. 使用MediaExtractor接口拿到mp4的第一帧码流
+2. 使用MediaCodec接口将一帧码流解码
+3. 使用MediaCodec接口将一帧码流解码并显示到窗口上
